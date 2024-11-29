e853d0647368e443cb8ff4297ffb2fb9da19f827
chim7a <chimalawrence11@gmail.com>
2024-11-29 17:15:19 +0100

    set up complete

---
+++ src/components/players.js
@@ -0,0 +1,43 @@
+// players.js
+
+const Players = [
+  {
+    name: "Cristiano Ronaldo",
+    team: "Manchester United",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 23, // In 2008
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Cristiano_Ronaldo_2018.jpg/800px-Cristiano_Ronaldo_2018.jpg",
+  },
+  {
+    name: "Lionel Messi",
+    team: "Barcelona",
+    nationality: "Argentinian",
+    jerseyNumber: 10,
+    age: 21, // In 2008
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a7/Lionel_Messi_2018.jpg",
+  },
+  {
+    name: "Wayne Rooney",
+    team: "Manchester United",
+    nationality: "English",
+    jerseyNumber: 10,
+    age: 22, // In 2008
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/a/af/Wayne_Rooney_2014.jpg",
+  },
+  {
+    name: "Kaka",
+    team: "AC Milan",
+    nationality: "Brazilian",
+    jerseyNumber: 22,
+    age: 26, // In 2008
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Kak%C3%A1_2013.jpg/800px-Kak%C3%A1_2013.jpg",
+  },
+];
+
+// Exporting the players array to be used in other parts of the application
+export default Players;
